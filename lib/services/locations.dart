a81c2d1b47a203cf615be484eaa083c4b74174af
Radical <romitradical@protonmail.com>
2019-09-29 15:36:16 +0530

    added locations and flags

---
+++ lib/services/locations.dart
@@ -0,0 +1,320 @@
+const flagURLstart = "https://www.countryflags.io";
+const flagURLend = "shiny/64.png";
+
+List locations = [
+  {
+    'location': 'Adelaide',
+    'url': 'Australia/Adelaide',
+    'flag': '$flagURLstart/au/$flagURLend'
+  },
+  {
+    'location': 'Amman',
+    'url': 'Asia/Amman',
+    'flag': '$flagURLstart/jo/$flagURLend'
+  },
+  {
+    'location': 'Baghdad',
+    'url': 'Asia/Baghdad',
+    'flag': '$flagURLstart/iq/$flagURLend'
+  },
+  {
+    'location': 'Bangkok',
+    'url': 'Asia/Bangkok',
+    'flag': '$flagURLstart/th/$flagURLend'
+  },
+  {
+    'location': 'Beirut',
+    'url': 'Asia/Beirut',
+    'flag': '$flagURLstart/lb/$flagURLend'
+  },
+  {
+    'location': 'Belize',
+    'url': 'America/Belize',
+    'flag': '$flagURLstart/bz/$flagURLend'
+  },
+  {
+    'location': 'Bermuda',
+    'url': 'Atlantic/Bermuda',
+    'flag': '$flagURLstart/bm/$flagURLend'
+  },
+  {
+    'location': 'Brisbane',
+    'url': 'Australia/Brisbane',
+    'flag': '$flagURLstart/au/$flagURLend'
+  },
+  {
+    'location': 'Brunei',
+    'url': 'Asia/Brunei',
+    'flag': '$flagURLstart/bn/$flagURLend'
+  },
+  {
+    'location': 'Bogota',
+    'url': 'America/Bogota',
+    'flag': '$flagURLstart/co/$flagURLend'
+  },
+  {
+    'location': 'Buenos Aires',
+    'url': 'America/Argentina/Buenos_Aires',
+    'flag': '$flagURLstart/ar/$flagURLend'
+  },
+  {
+    'location': 'Cairo',
+    'url': 'Africa/Cairo',
+    'flag': '$flagURLstart/eg/$flagURLend'
+  },
+  {
+    'location': 'Cancun',
+    'url': 'America/Cancun',
+    'flag': '$flagURLstart/mx/$flagURLend'
+  },
+  {
+    'location': 'Caracas',
+    'url': 'America/Caracas',
+    'flag': '$flagURLstart/ve/$flagURLend'
+  },
+  {
+    'location': 'Chicago',
+    'url': 'America/Chicago',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Colombo',
+    'url': 'Asia/Colombo',
+    'flag': '$flagURLstart/lk/$flagURLend'
+  },
+  {
+    'location': 'Costa Rica',
+    'url': 'America/Costa_Rica',
+    'flag': '$flagURLstart/cr/$flagURLend'
+  },
+  {
+    'location': 'Damascus',
+    'url': 'Asia/Damascus',
+    'flag': '$flagURLstart/sy/$flagURLend'
+  },
+  {
+    'location': 'Denver',
+    'url': 'America/Denver',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Detroit',
+    'url': 'America/Detroit',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Dhaka',
+    'url': 'Asia/Dhaka',
+    'flag': '$flagURLstart/bd/$flagURLend'
+  },
+  {
+    'location': 'Dubai',
+    'url': 'Asia/Dubai',
+    'flag': '$flagURLstart/ae/$flagURLend'
+  },
+  {
+    'location': 'El Salvador',
+    'url': 'America/El_Salvador',
+    'flag': '$flagURLstart/sv/$flagURLend'
+  },
+  {
+    'location': 'Gaza',
+    'url': 'Asia/Gaza',
+    'flag': '$flagURLstart/ps/$flagURLend'
+  },
+  {
+    'location': 'Guatemala',
+    'url': 'America/Guatemala',
+    'flag': '$flagURLstart/gt/$flagURLend'
+  },
+  {
+    'location': 'Havana',
+    'url': 'America/Havana',
+    'flag': '$flagURLstart/cu/$flagURLend'
+  },
+  {
+    'location': 'Hong Kong',
+    'url': 'Asia/Hong_Kong',
+    'flag': '$flagURLstart/hk/$flagURLend'
+  },
+  {
+    'location': 'Jakarta',
+    'url': 'Asia/Jakarta',
+    'flag': '$flagURLstart/id/$flagURLend'
+  },
+  {
+    'location': 'Jamaica',
+    'url': 'America/Jamaica',
+    'flag': '$flagURLstart/jm/$flagURLend'
+  },
+  {
+    'location': 'Jerusalem',
+    'url': 'Asia/Jerusalem',
+    'flag': '$flagURLstart/il/$flagURLend'
+  },
+  {
+    'location': 'Johannesburg',
+    'url': 'Africa/Johannesburg',
+    'flag': '$flagURLstart/za/$flagURLend'
+  },
+  {
+    'location': 'Kabul',
+    'url': 'Asia/Kabul',
+    'flag': '$flagURLstart/af/$flagURLend'
+  },
+  {
+    'location': 'Karachi',
+    'url': 'Asia/Karachi',
+    'flag': '$flagURLstart/pk/$flagURLend'
+  },
+  {
+    'location': 'Kathmandu',
+    'url': 'Asia/Kathmandu',
+    'flag': '$flagURLstart/np/$flagURLend'
+  },
+  {
+    'location': 'Kolkata',
+    'url': 'Asia/Kolkata',
+    'flag': '$flagURLstart/in/$flagURLend'
+  },
+  {
+    'location': 'Kuala Lumpur',
+    'url': 'Asia/Kuala_Lumpur',
+    'flag': '$flagURLstart/my/$flagURLend'
+  },
+  {
+    'location': 'Lagos',
+    'url': 'Africa/Lagos',
+    'flag': '$flagURLstart/ng/$flagURLend'
+  },
+  {
+    'location': 'Los Angeles',
+    'url': 'America/Los_Angeles',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Louisville',
+    'url': 'America/Kentucky/Louisville',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Manila',
+    'url': 'Asia/Manila',
+    'flag': '$flagURLstart/ph/$flagURLend'
+  },
+  {
+    'location': 'Melbourne',
+    'url': 'Australia/Melbourne',
+    'flag': '$flagURLstart/au/$flagURLend'
+  },
+  {
+    'location': 'Mexico City',
+    'url': 'America/Mexico_City',
+    'flag': '$flagURLstart/mx/$flagURLend'
+  },
+  {
+    'location': 'Nairobi',
+    'url': 'Africa/Nairobi',
+    'flag': '$flagURLstart/ke/$flagURLend'
+  },
+  {
+    'location': 'New York',
+    'url': 'America/New_York',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Panama',
+    'url': 'America/Panama',
+    'flag': '$flagURLstart/pa/$flagURLend'
+  },
+  {
+    'location': 'Perth',
+    'url': 'Australia/Perth',
+    'flag': '$flagURLstart/au/$flagURLend'
+  },
+  {
+    'location': 'Phoenix',
+    'url': 'America/Phoenix',
+    'flag': '$flagURLstart/us/$flagURLend'
+  },
+  {
+    'location': 'Puerto Rico',
+    'url': 'America/Puerto_Rico',
+    'flag': '$flagURLstart/pr/$flagURLend'
+  },
+  {
+    'location': 'Pyongyang',
+    'url': 'Asia/Pyongyang',
+    'flag': '$flagURLstart/kp/$flagURLend'
+  },
+  {
+    'location': 'Reykjavik',
+    'url': 'Atlantic/Reykjavik',
+    'flag': '$flagURLstart/is/$flagURLend'
+  },
+  {
+    'location': 'Riyadh',
+    'url': 'Asia/Riyadh',
+    'flag': '$flagURLstart/sa/$flagURLend'
+  },
+  {
+    'location': 'Seoul',
+    'url': 'Asia/Seoul',
+    'flag': '$flagURLstart/kr/$flagURLend'
+  },
+  {
+    'location': 'Shanghai',
+    'url': 'Asia/Shanghai',
+    'flag': '$flagURLstart/cn/$flagURLend'
+  },
+  {
+    'location': 'Singapore',
+    'url': 'Asia/Singapore',
+    'flag': '$flagURLstart/sg/$flagURLend'
+  },
+  {
+    'location': 'Sydney',
+    'url': 'Australia/Sydney',
+    'flag': '$flagURLstart/au/$flagURLend'
+  },
+  {
+    'location': 'Taipei',
+    'url': 'Asia/Taipei',
+    'flag': '$flagURLstart/tw/$flagURLend'
+  },
+  {
+    'location': 'Tashkent',
+    'url': 'Asia/Tashkent',
+    'flag': '$flagURLstart/uz/$flagURLend'
+  },
+  {
+    'location': 'Tehran',
+    'url': 'Asia/Tehran',
+    'flag': '$flagURLstart/ir/$flagURLend'
+  },
+  {
+    'location': 'Tijuana',
+    'url': 'America/Tijuana',
+    'flag': '$flagURLstart/mx/$flagURLend'
+  },
+  {
+    'location': 'Tokyo',
+    'url': 'Asia/Tokyo',
+    'flag': '$flagURLstart/jp/$flagURLend'
+  },
+  {
+    'location': 'Toronto',
+    'url': 'America/Toronto',
+    'flag': '$flagURLstart/ca/$flagURLend'
+  },
+  {
+    'location': 'Tripoli',
+    'url': 'Africa/Tripoli',
+    'flag': '$flagURLstart/ly/$flagURLend'
+  },
+  {
+    'location': 'Vancouver',
+    'url': 'America/Vancouver',
+    'flag': '$flagURLstart/ca/$flagURLend'
+  }
+];
